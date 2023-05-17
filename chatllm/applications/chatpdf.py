5a9994aab23b4d1f1bb6c08bca0d2a94dce64d3c
Huajun Bai <baihuajun24@gmail.com>
2023-05-17 13:51:22 +0800

    local run succeed

--- chatllm/applications/chatpdf.py
+++ chatllm/applications/chatpdf.py
@@ -36,10 +36,19 @@ if __name__ == '__main__':
     from chatllm.applications.chatpdf import ChatPDF
 
     qa = ChatPDF(encode_model='nghuyong/ernie-3.0-nano-zh')  # 自动建索引
-    qa.create_index('../../data/财报.pdf')
-    qa.load_llm4chat(model_name_or_path='/Users/betterme/PycharmProjects/AI/CHAT_MODEL/chatglm', device='cpu')
+    # qa.create_index('../../data/财报.pdf')
+    # qa.load_llm4chat(model_name_or_path='/Users/betterme/PycharmProjects/AI/CHAT_MODEL/chatglm', device='cpu')
+    # qa.create_index('data/财报.pdf')
+    source = 'data/接种率文献/应用Kaplan-Meie...市首剂含麻疹成份疫苗接种率_王萌.pdf'
+    query = '延迟接种属于疫苗犹豫吗？'
+    qa.create_index(source)
+    # AssertionError: Torch not compiled with CUDA enabled
+    qa.load_llm4chat(model_name_or_path='D:\huajun\chatGLM\chatGLM\chatglm-6B', device='cpu')
 
-    list(qa(query='东北证券主营业务', topk=1, threshold=0.8))
+    # list(qa(query='东北证券主营业务', topk=1, threshold=0.8))
+    list(qa(query=query, topk=1, threshold=0.8))
 
     # 召回结果
+    print(f"Ask source is: {source}")
+    print(f"query is {query}")
     print(qa.recall)
